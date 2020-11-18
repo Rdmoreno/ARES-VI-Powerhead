5d24de9380c6f6eceb89c7dfe73bc258c3000b8c
Ryan Moreno <rdmoreno@crimson.ua.edu>
2020-11-17 18:42:06 -0800

    Initial Upload

--- sensors.py
+++ sensors.py
@@ -10,6 +10,23 @@ from random import randint
 class Sensor:
     def __init__(self, given_name, given_type,
                  given_pin0, given_pin1, given_pin2):
+        """
+        Read data from sensor
+
+        description:
+            Takes sensor information including name, type, and pin locations to read the sensor and return the
+            average of the sensors after voting.
+
+        :param given_name: Name of the group of sensors
+        :param given_type: Type of Sensor
+        :param given_pin0: The first pin in the group
+        :param given_pin1: The second pin in the group
+        :param given_pin2: The third pin in the group
+
+        :return: avg: Float, the average of the sensor data, after running through the vote function.
+        """
+
+        # Given sensor information added to class self type for function use
         self.name = given_name
         self.type = given_type
         self.pin0 = given_pin0
@@ -19,15 +36,14 @@ class Sensor:
         self.avg_data = []
 
     def read_pressure(self):
-        """"
-        Read data from pressure sensor
-        
-        description blah vail
-        
-        :returns
-        avg: Float, 
-            the average of the sensor data, after running through the 
-            vote function.        
+        """
+       Read data from pressure sensor
+
+        description:
+            Reads the pressure sensors and returns them to the main function after converting from volts to psi and
+            averaging.
+
+        :return: avg: Float, the average of the sensor data, after running through the vote function.
         """
 
         # Streaming data from CSV to simulate sensor reading
@@ -37,41 +53,94 @@ class Sensor:
 
         # Recording sensor time
         t = time.process_time()
-        # print(t)
+
         # Placing sensor data into numpy array
         temps = [t, randint(1, 20), randint(1, 20), randint(1, 20)]
 
         # Appending numpy array to data list
         self.data.append(temps)
-        # print(self.data)
+        print(self.data)
 
         # fetching voted average
         avg = self.vote(temps[1::])
 
+        # Appends temporary data to sensor data array
         self.data.append(np.array(temps))
         self.avg_data.append(avg)
 
+        # Returns average sensor reading to the main function
         return avg
 
     # noinspection PyMethodMayBeStatic
     def average(self, temps):
+        """
+        Averages the data from sensors
+
+        description:
+            Averages the readings from the three sensors before and after voting occurs
+
+        :param temps: Array of the operation time and the three sensor readings
+
+        :return:
+            avg: Float, the average of the sensor data, after running through the vote function.
+        """
+
+        # Averages the current temporary sensor data array and returns it to the vote function
         average = sum(temps) / len(temps)
         return average
 
     def vote(self, temps):
+        """
+        Voting of the three sensor readings
+
+        description:
+            Votes and removes the sensor most deviating from the average of the three sensor readings.
+
+        :param temps: Array of the operation time and the three sensor readings
+
+        :return: average(temps): the final average after voting
+
+        """
+
+        # Averages the given temporary three sensor data array and votes/removes the sensor deviating furthest from the
+        # average.
         list_avg = self.average(temps)
         difference = [abs(list_avg - temps[0]), abs(list_avg - temps[1]),
                       abs(list_avg - temps[2])]
-
         del temps[difference.index(max(difference))]
+
+        # Returns the average of the remaining two sensor readings as the final sensor reading
         return self.average(temps)
 
     # noinspection PyMethodMayBeStatic
     def volt_to_psi(self, val):
+        """
+        Convert pressure sensor reading
+
+        description:
+            Converts given pressure sensor reading from volts to psi
+
+        :param val: The reading from the three pressure sensors in volts
+
+        :return:
+            The given pressure sensor reading in psi
+        """
+
+        # Returns the pressure sensor reading in psi using the equation listed below
         return 1715.465955 * (val * 1.8) - 312.506433
 
     def save_data(self):
+        """
+        Record sensor data
+
+        description:
+            Records the data from the current iteration of the sensor reading into a excel file.
+        """
+
+        # Takes raw sensor data readings and appends them into the excel document
         data_df = pd.DataFrame(self.data, columns=['time', 'p0', 'p1', 'p2'])
         data_df.to_excel('raw_data.xlsx')
+
+        # Takes the average sensor reading after voting and appends them into an excel document
         avg_df = pd.DataFrame(self.avg_data, columns=['avg'])
         avg_df.to_excel('avg_data.xlsx')
